2832649aafe17073a0ca5baf9cd52bfb327ac0ef
Franklin carrero <franklin.carrero@ceiba.com.co>
2019-03-10 11:07:18 -0500

    add header segurity

---
+++ src/main/java/com/ceiba/demo/infrastructure/application/springapp/config/security/WebSecurityConfig.java
@@ -0,0 +1,15 @@
+package com.ceiba.demo.infrastructure.application.springapp.config.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.headers()
+            .contentSecurityPolicy("script-src 'self' https://trustedscripts.example.com; object-src https://trustedplugins.example.com; report-uri /csp-report-endpoint/");
+    }
+}
